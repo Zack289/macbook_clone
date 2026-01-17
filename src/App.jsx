b6871875885b30015e717d224126bf50c5c2b448
Zack289 <ozack911@gmail.com>
2026-01-17 12:54:10 +0545

    Created the productViewer component and converted the 3d model into a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import ProductViewer from './components/ProductViewer'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
 
     <Navbar />
     <Hero />
+    <ProductViewer />
 
    </main>
   )
